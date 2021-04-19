d4d909551ac0fb03671b7ae027123d259129c317
Vinicius Campos da Silva <vini.campos.silva@gmail.com>
2021-04-19 20:33:01 -0300

    Ticket: Get Comments

--- M220N/Repositories/MoviesRepository.cs
+++ M220N/Repositories/MoviesRepository.cs
@@ -86,9 +86,12 @@ namespace M220N.Repositories
             {
                 return await _moviesCollection.Aggregate()
                     .Match(Builders<Movie>.Filter.Eq(x => x.Id, movieId))
-                    // Ticket: Get Comments
-                    // Add a lookup stage that includes the
-                    // comments associated with the retrieved movie
+                    .Lookup(
+                    _commentsCollection, 
+                    m => m.Id,
+                    c => c.MovieId,
+                    (Movie m)=> m.Comments
+                    )
                     .FirstOrDefaultAsync(cancellationToken);
             }
 
